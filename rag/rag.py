9c58542086fdecede7601a93a98261920c9264f4
Md Nazmul Hasan <farhapartex@gmail.com>
2025-06-18 01:37:54 +0600

    (feat) implemented MultiQueryRAG

--- rag/rag.py
+++ rag/rag.py
@@ -1,9 +1,10 @@
 from typing import Dict, List
 
-from langchain.retrievers import ParentDocumentRetriever, MultiQueryRetriever
+from langchain.retrievers.parent_document_retriever import ParentDocumentRetriever
+from langchain.retrievers.multi_query import MultiQueryRetriever
 from langchain.schema import Document
 from langchain_core.stores import InMemoryStore
-from langchain_openai import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 
@@ -36,8 +37,7 @@ class ParentDocumentRAG:
             vectorstore=vectorstore,
             docstore=store,
             child_splitter=child_splitter,
-            parent_splitter=parent_splitter,
-            k=3, # return top 3 parent documents
+            parent_splitter=parent_splitter
         )
         retriever.add_documents(documents)
         self.vectorstore = vectorstore
@@ -49,10 +49,29 @@ class ParentDocumentRAG:
 
         results = self.retriever.get_relevant_documents(query)
 
-        for i, doc in enumerate(results):
-            print(f"{i + 1}. Content length: {len(doc.page_content)} chars")
-            print(f"   Metadata: {doc.metadata}")
-            print(f"   Preview: {doc.page_content[:100]}...")
-            print("-" * 40)
+        # for i, doc in enumerate(results):
+        #     print(f"{i + 1}. Content length: {len(doc.page_content)} chars")
+        #     print(f"   Metadata: {doc.metadata}")
+        #     print(f"   Preview: {doc.page_content[:100]}...")
+        #     print("-" * 40)
 
+        return results
+
+class MultiQueryRAG:
+    def __init__(self, vectorstore: Chroma, llm: ChatOpenAI):
+        self.vectorstore = vectorstore
+        self.llm = llm
+
+    def create_multi_query_retriever(self) -> MultiQueryRetriever:
+        base_retriever = self.vectorstore.as_retriever(search_kwargs={"k": 4})
+        multi_query_retriever = MultiQueryRetriever.from_llm(
+            retriever=base_retriever,
+            llm=self.llm
+        )
+
+        return multi_query_retriever
+
+    def search_with_multiple_queries(self, query: str) -> List[Document]:
+        retriever = self.create_multi_query_retriever()
+        results = retriever.get_relevant_documents(query)
         return results
